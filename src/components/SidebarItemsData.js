09b727a654bda4697eb05e6f017717d635deea41
MalC-U <mwcalow@gmail.com>
2021-01-12 10:30:35 +0000

    Added links that will be used in a sidebar

---
+++ src/components/SidebarItemsData.js
@@ -0,0 +1,22 @@
+const SidebarItemsData = [
+    {
+        id: 1,
+        name: "github",
+        itemUrl: "https://github.com/malc-u",
+        itemIcon: "fab fa-github sidebar-item-icon",
+    },
+    {
+        id: 2,
+        name: "linkedin",
+        itemUrl: "https://www.linkedin.com/in/mal-c-u/",
+        itemIcon: "fab fa-linkedin-in sidebar-item-icon",
+    },
+    {
+        id: 3,
+        name: "mail",
+        itemUrl: "mailto:mwcalow@gmail.com",
+        itemIcon: "far fa-envelope sidebar-item-icon",
+    },
+]
+
+export default SidebarItemsData;
