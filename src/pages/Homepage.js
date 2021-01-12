d7ae1c680238abf7576ba65d6ed662076c17b3fe
MalC-U <mwcalow@gmail.com>
2021-01-12 10:35:22 +0000

    Created homepage

---
+++ src/pages/Homepage.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+class Homepage extends React.Component {
+  render() {
+    return (
+      <>
+        <div className="page container-fluid">
+
+            <h1>Page</h1>
+
+        </div>
+      </>
+    );
+  }
+}
+
+export default Homepage;
