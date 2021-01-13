833f329a556adb3e2b4868fbdfca7b37c3188c24
MalC-U <mwcalow@gmail.com>
2021-01-13 20:33:35 +0000

    Amended how Credential items are displayed on small screens

--- src/components/CredentialItem.js
+++ src/components/CredentialItem.js
@@ -3,7 +3,7 @@ import React from "react";
 const CredentialItem = ({id, type, name, itemUrl, date, body}) => {
     return (
         <>
-        <div className="col-sm-12 col-md-3 card">
+        <div className="col-sm-6 col-md-3 card">
             <div className="text-center card-body">
                 <p className="small">{type}</p>
                 <a className="btn btn-outline-info my-2" href={itemUrl} target="_blank" rel="noopener noreferrer">{name}</a>
