3fec95babc8c5255f20c9bb5bf91ba8f24dac9e8
MalC-U <mwcalow@gmail.com>
2021-01-13 20:18:16 +0000

    Created CredentialItem and how it is going to be displayed

---
+++ src/components/CredentialItem.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const CredentialItem = ({id, type, name, itemUrl, date, body}) => {
+    return (
+        <>
+        <div className="col-sm-12 col-md-3 card">
+            <div className="text-center card-body">
+                <p className="small">{type}</p>
+                <a className="btn btn-outline-info my-2" href={itemUrl} target="_blank" rel="noopener noreferrer">{name}</a>
+                <p className="small">{body} - {date}</p>
+
+            </div>
+        </div>
+        </>
+    )
+}
+
+export default CredentialItem;
