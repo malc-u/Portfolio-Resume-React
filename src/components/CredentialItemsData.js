285f5148cc4cc7722fcf252530ffe66c39550b42
MalC-U <mwcalow@gmail.com>
2021-01-13 20:17:50 +0000

    Amended credential item details

--- src/components/CredentialItemsData.js
+++ src/components/CredentialItemsData.js
@@ -2,7 +2,7 @@ const CredentailItemsData = [
     {
         id: 1,
         type: "Diploma",
-        name: "Diploma in Software Development",
+        name: "Software Development",
         itemUrl: "https://api.accredible.com/v1/frontend/credential_website_embed_image/certificate/25778440",
         date: "November 2020",
         body: "Code Institute",
