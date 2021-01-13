84edc20772dfe878562daf1b763a053348422902
MalC-U <mwcalow@gmail.com>
2021-01-13 21:12:13 +0000

    created ortfolio items data to be used to display portfolio

---
+++ src/components/PortfolioItemsData.js
@@ -0,0 +1,45 @@
+const PortfolioItemsData = [
+    {
+        id: 1,
+        name: "Fostoria",
+        descripton: "Landscape photography page with webshop",
+        tools: "django, python"
+        itemUrl: "https://fostoria.herokuapp.com/",
+        itemImg: "https://i.ibb.co/C1SkTgs/mockup.jpg",
+    },
+    {
+        id: 2,
+        name: "Someone Knows Something",
+        descripton: "British, Australian and American True Crime Podcasts website",
+        tools: "flask, python",
+        itemUrl: "https://someone-knows-something.herokuapp.com/",
+        itemImg: "https://i.ibb.co/dp0bJ2v/multidevice.png",
+    },
+    {
+        id: 3,
+        name: "Awesome 80s - memory card game",
+        descripton: "Card matching game",
+        tools: "javascript, jquery",
+        itemUrl: "https://malc-u.github.io/memoryCardGame/",
+        itemImg: "https://i.ibb.co/PQDc57k/game.jpg",
+    },
+    {
+        id: 3,
+        name: "Top Gear Strzelin",
+        descripton: "Car garage website",
+        tools: "html, css, javascript",
+        itemUrl: "https://malc-u.github.io/top-gear-strzelin/",
+        itemImg: "https://i.ibb.co/1nk1Lfj/web.jpg",
+    },
+    {
+        id: 5,
+        name: "Pete's Plumbing",
+        descripton: "Plumbing company website",
+        tools: "html, css, javascript",
+        itemUrl: "https://malc-u.github.io/petes-plumbing/",
+        itemImg: "https://i.ibb.co/vV7knZM/page.jpg",
+    },
+
+]
+
+export default PortfolioItemsData;
