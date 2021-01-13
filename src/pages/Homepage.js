aa3a52d6f514cf6aeee5b0fe9bd2f7864cad7169
MalC-U <mwcalow@gmail.com>
2021-01-13 17:46:35 +0000

    Updated Homepage - name and title

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,12 +1,18 @@
 import React from "react";
 
 class Homepage extends React.Component {
+  
+ 
   render() {
     return (
       <>
         <div className="page container-fluid">
-
-            <h1>Page</h1>
+          <div className="row">
+            <div className="col-12 text-center">
+              <h1 className="p-3 display-3">Mal Calow-Uttley</h1>
+              <h3>Junior Software Developer</h3>
+            </div>
+          </div>
 
         </div>
       </>
