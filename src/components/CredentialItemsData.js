b8347a2aedf7d28c47e3323f95e6c728cb625bf3
MalC-U <mwcalow@gmail.com>
2021-01-13 17:45:35 +0000

    Created Credential Items Data that will be used to display details

---
+++ src/components/CredentialItemsData.js
@@ -0,0 +1,52 @@
+const CredentailItemsData = [
+    {
+        id: 1,
+        type: "Diploma",
+        name: "Diploma in Software Development",
+        itemUrl: "https://api.accredible.com/v1/frontend/credential_website_embed_image/certificate/25778440",
+        date: "November 2020",
+        body: "Code Institute",
+    },
+    {
+        id: 2,
+        name: "React - The Big Picture",
+        type: "Certificate of completion",
+        itemUrl: "https://certificates-and-credentials.s3.eu-west-2.amazonaws.com/React-+The+Big+Picture.pdf",
+        date: "October 2020",
+        body: "Pluralsight",
+    },
+    {
+        id: 3,
+        name: "React Fundamentals",
+        type: "Certificate of completion",
+        itemUrl: "https://certificates-and-credentials.s3.eu-west-2.amazonaws.com/React+Fundamentals.pdf",
+        date: "November 2020",
+        body: "Pluralsight",
+    },
+    {
+        id: 4,
+        name: "Building Applications with React and Redux",
+        type: "Certificate of completion",
+        itemUrl: "https://certificates-and-credentials.s3.eu-west-2.amazonaws.com/Building+Applications+with+React+and+Redux.pdf",
+        date: "December 2020",
+        body: "Pluralsight",
+    },
+    {
+        id: 5,
+        name: "Docker and Kubernetes The Big Picture",
+        type: "Certificate of completion",
+        itemUrl: "https://certificates-and-credentials.s3.eu-west-2.amazonaws.com/Docker+and+Kubernetes+The+Big+Picture.pdf",
+        date: "January 2021",
+        body: "Pluralsight",
+    },
+    {
+        id: 6,
+        name: "Getting Started With Docker",
+        type: "Certificate of completion",
+        itemUrl: "https://certificates-and-credentials.s3.eu-west-2.amazonaws.com/Getting+Started+With+Docker.pdf",
+        date: "January 2021",
+        body: "Pluralsight",
+    },
+]
+
+export default CredentailItemsData;
