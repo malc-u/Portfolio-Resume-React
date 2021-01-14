24010a697bb43299d6ee3c82cc9b4f41ed3501bd
MalC-U <mwcalow@gmail.com>
2021-01-14 20:11:39 +0000

    Included Portfolio on the Homepage +imported it

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Credentials from "../components/Credentials";
+import Portfolio from "../components/Portfolio";
 
 class Homepage extends React.Component {
   
@@ -10,10 +11,11 @@ class Homepage extends React.Component {
         <div className="page container-fluid">
           <div className="row pb-3">
             <div className="col-12 text-center p-3">
-              <h1 className="p-3">Mal Calow-Uttley</h1>
+              <h1 className="p-3 text-info">Mal Calow-Uttley</h1>
               <h3>Junior Software Developer</h3>
             </div>
           </div>
+          <Portfolio />
           <Credentials />
 
         </div>
