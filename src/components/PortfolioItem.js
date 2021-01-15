a9ba83494e648ef55c0a8a9a0e6cb6f409379f27
MalC-U <mwcalow@gmail.com>
2021-01-15 11:05:11 +0000

    Added Portfolio Item dispaly

--- src/components/PortfolioItem.js
+++ src/components/PortfolioItem.js
@@ -1,8 +1,17 @@
 import React from "react";
 
-const PortfolioItem = ({id, name, descripton, tools, itemUrl, itemImg}) => {
+const PortfolioItem = ({id, name, description, tools, itemUrl, itemImg}) => {
     return(
         <>
+        <div className="col-sm-12 col-md-3 m-2 btn btn-outline-info" id="portfolio-item">
+            <div className="col-sm-12 py-2">
+                <a href={itemUrl}>
+                   <div className="portfolio-img">
+                        <img className="card-img-top" src={itemImg} alt={name}/>
+                   </div>
+                </a>
+            </div>
+        </div>
         </>
     );
 }
